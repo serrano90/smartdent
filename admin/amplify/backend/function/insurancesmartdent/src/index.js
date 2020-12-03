9af54241d3379568d10be36d6736e8c610afdbaa
Andros Serrano <serrano.hernandez90@gmail.com>
2020-12-03 02:04:03 -0500

    feat: change index and parameters

--- admin/amplify/backend/function/insurancesmartdent/src/index.js
+++ admin/amplify/backend/function/insurancesmartdent/src/index.js
@@ -1,9 +1,15 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_SMARTDENTCARDINTENT_ARN
+	STORAGE_SMARTDENTCARDINTENT_NAME
+	STORAGE_SMARTDENTCUSTOMER_ARN
+	STORAGE_SMARTDENTCUSTOMER_NAME
+Amplify Params - DO NOT EDIT */const awsServerlessExpress = require('aws-serverless-express');
+const httpServer = require('./interfaces/http/server');
+
+const { app } = httpServer()
 
 const server = awsServerlessExpress.createServer(app);
 
-exports.handler = (event, context) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
-};
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
